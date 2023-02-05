5fc00d0f80f50289556c17a6d7490b658c4cabda
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2023-02-05 22:57:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -122,17 +122,32 @@ const handlerRequest = (url, req, res) => {
         {
           "id": "1000000000000000000",
           "type": 0,
-          "content": `**Word From Developers**
-Thanks for using our client! <:Kanna_Heart:882480441075040257> 
-I started this as a hobby project and stayed since ya'll loved it. <:TeriSmile:1048682023839088640> 
-And I kid you not, I've never had these many users before!? <a:aqua:857071030689202196>
-<a:mikupaylak:863287070407786516> If you had fun, please leave a star on the repo <:github:889092230063734795> 
-https://github.com/aiko-chan-ai/DiscordBotClient
-It really motivates me to work on the project! <:elylove:1065888090549407785>`,
           "channel_id": "1000000000000000000",
           "author": UserData,
           "attachments": [],
-          "embeds": [],
+          "content": null,
+          "embeds": [
+            {
+              "type": "rich",
+              "description": "Thanks for using our client! <:Kanna_Heart:882480441075040257> \nI started this as a hobby project and stayed since ya'll loved it. <:TeriSmile:1048682023839088640> \nAnd I kid you not, I've never had these many users before!? <a:aqua:857071030689202196>\n\n<a:mikupaylak:863287070407786516> If you had fun, please leave a star on the [repo](https://github.com/aiko-chan-ai/DiscordBotClient) <:github:889092230063734795> \nIt really motivates me to work on the project! <:elylove:1065888090549407785>",
+              "color": 16750296,
+              "timestamp": "2022-12-31T17:00:00+00:00",
+              "author": {
+                "name": "Word From Developers",
+                "icon_url": "https://cdn.discordapp.com/attachments/878276279105884210/1071796241765240862/LINE_nachonekodayo_005.png",
+                "proxy_icon_url": "https://media.discordapp.net/attachments/878276279105884210/1071796241765240862/LINE_nachonekodayo_005.png"
+              },
+              "thumbnail": {
+                "url": "https://cdn.discordapp.com/avatars/1056491867375673424/93fb88f6b8c0a2a33c437d0fff4c6625.png",
+                "proxy_url": "https://images-ext-2.discordapp.net/external/BbiJ0GLoefkW4PlPBsmxh7GfKALUE2XI2Ax7fFLCIo4/https/cdn.discordapp.com/avatars/1056491867375673424/93fb88f6b8c0a2a33c437d0fff4c6625.png",
+                "width": 128,
+                "height": 128
+              },
+              "footer": {
+                "text": "Elysia"
+              }
+            }
+          ],
           "mentions": [],
           "mention_roles": [],
           "pinned": false,

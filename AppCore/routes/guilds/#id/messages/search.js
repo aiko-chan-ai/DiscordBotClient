8008b6b426fa1b75a4b87f34aaca1386cf431586
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2024-03-24 14:17:08 +0700

    Update search.js

--- AppCore/routes/guilds/#id/messages/search.js
+++ AppCore/routes/guilds/#id/messages/search.js
@@ -1,19 +1,20 @@
 const { Router } = require('express');
 const crypto = require('node:crypto');
-
 const app = Router();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 	const salt = Math.random().toString();
 	const hash = crypto
 		.createHash('md5')
-		.update(salt + "Elysia")
+		.update(salt + 'Elysia')
 		.digest('hex');
 	return res.status(200).send({
 		analytics_id: hash,
 		doing_deep_historical_index: false,
 		total_results: 0,
 		messages: [],
+		members: [],
+		threads: [],
 	});
 });
 
